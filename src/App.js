1999dd244134748eafa6ed6b07add114db605d30
Jahid Hassan <jahidhassan.programmer@gmail.com>
2022-05-21 21:34:09 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' ></Route>
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App;
