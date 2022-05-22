2ac071bd1372d8776c286ee3092d187f82f69f05
Jahid Hassan <jahidhassan.programmer@gmail.com>
2022-05-22 21:41:49 +0600

    Error on quantity

---
+++ src/Pages/Purchase/ProductDetails.js
@@ -0,0 +1,77 @@
+import React, { useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import './ProductDetails.css'
+
+const ProductDetails = ({ product }) => {
+    const { name, brand, category, description, minOrder, price, itemSold, availableQty } = product;
+    const [totalPrice, setTotalPrice] = useState(minOrder * 10);
+    const [Qty, setQty] = useState(minOrder);
+    const [qtyError, setQtyError] = useState('');
+
+    const handleQty = (e) => {
+        e.preventDefault();
+        setQtyError('');
+        const value = (e.target.qty.value);
+        setTotalPrice(value * price)
+        setQty(parseInt(value));
+    }
+    console.log('Less Qty: ', Qty);
+    console.log('availableQty: ', availableQty);
+
+    if ((Qty < minOrder) || (Qty > availableQty)) {
+        setQtyError('Please see product quantity and minimum order');
+        setQty(availableQty);
+    }
+
+    return (
+        <div className='grid grid-cols-2 '>
+            <div className='productDetails-body'>
+                <h2 className="card-title text-2xl uppercase">{name}</h2>
+                <p className=" text-2xl font-semibold ">Unit Price: <b>$ {price}</b></p>
+                <p className=" text-lg font-semibold">Minimum Order: <b>{minOrder}</b> pcs</p>
+                <p className=" text-lg font-semibold">Sold: <b>{itemSold}</b> pcs</p>
+                <p className=" text-lg font-semibold">Available: <b>{availableQty}</b> pcs</p>
+                <p className=" text-lg font-semibold">Category: <b className='uppercase'>{category}</b> </p>
+                <p className=" text-lg font-semibold">Brand: <b className='uppercase'>{brand}</b> </p>
+                <p><span className='text-lg font-semibold'>Rating: </span>
+                    <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
+                    <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
+                    <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
+                    <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
+                    <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
+                </p>
+                <p className='text-lg '>{description}</p>
+
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+                <div className='flex flex-cols gap-200'>
+                    <div class=" ">
+                        <form onSubmit={handleQty} >
+                            <label class="label">
+                                <span class="label-text text-xl">Enter Quantity</span>
+                            </label>
+
+                            <label class="input-group">
+                                <input type="number" placeholder={`Min qty ${minOrder}`} name='qty' class=" border px-5 border-black text-lg" />
+                                <button className='btn text-lg' type="submit">Enter</button>
+                            </label>
+                        </form>
+                        {
+                            qtyError && <p className='text-xl mt-2 text-red-500'>{qtyError}</p>
+                        }
+                        <div className='my-5'>
+                            <p className='text-2xl text-center'>Total Price: {totalPrice}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <button className="btn btn-primary uppercase text-white font-bold text-xl">Order Now</button>
+                </div>
+
+            </div>
+        </div >
+    );
+};
+
+export default ProductDetails;
--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -4,6 +4,7 @@ import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import ProductDetails from './ProductDetails';
 
 const Purchase = () => {
     const { id } = useParams();
@@ -19,31 +20,17 @@ const Purchase = () => {
     if (isLoading) {
         return <Loading></Loading>
     }
-    const { _id, name, brand, category, img, description, minOrder, price, itemSold, availableQty } = product;
+
     return (
         <div className='container'>
-            <div class="hero mt-10">
+            <div class="hero mt-20">
                 <div class="flex flex-col lg:flex-row gap-10">
-                    <img src={img} class="max-w-xl rounded-lg shadow-2xl" />
+                    <img src={product.img} class="max-w-xl rounded-lg shadow-2xl" />
                     <div>
-                        <h2 className="card-title text-2xl uppercase">{name}</h2>
-                        <p className=" text-2xl font-semibold ">Unit Price: <b>$ {price}</b></p>
-                        <p className=" text-lg font-semibold">Minimum Order: <b>{minOrder}</b> pcs</p>
-                        <p className=" text-lg font-semibold">Sold: <b>{itemSold}</b> pcs</p>
-                        <p className=" text-lg font-semibold">Available: <b>{availableQty}</b> pcs</p>
-                        <p className=" text-lg font-semibold">Category: <b className='uppercase'>{category}</b> </p>
-                        <p className=" text-lg font-semibold">Brand: <b className='uppercase'>{brand}</b> </p>
-                        <p><span className='text-lg font-semibold'>Rating: </span>
-                            <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
-                            <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
-                            <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
-                            <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
-                            <FontAwesomeIcon className='text-primary text-xl' icon={faStar} />
-                        </p>
-                        <p className='text-lg '>{description}</p>
-                        <div className="card-actions justify-center">
-                            <button className="btn btn-primary uppercase text-white font-bold text-xl">Order Now</button>
-                        </div>
+                        <ProductDetails
+                            key={product._id}
+                            product={product}
+                        ></ProductDetails>
                     </div>
                 </div>
             </div>
