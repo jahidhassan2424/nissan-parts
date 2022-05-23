e3ffb31f44d919dafc6cc0e6d49f469df2e8957d
Jahid Hassan <jahidhassan.programmer@gmail.com>
2022-05-23 07:03:55 +0600

    before installing react hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,54 +1,87 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const Login = () => {
     const [forgetPass, setForgetPass] = useState(false);
     const [forgetPassText, setForgetPassText] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+    error && setPasswordError(error.message);
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        signInWithEmailAndPassword(email, password);
+
+    }
     return (
         <div>
             <div class=" flex justify-center items-center mt-20">
                 <div class=" w-1/4 flex-col lg:flex-row-reverse">
                     <div class="card flex-shrink-0 w-full  shadow-2xl bg-base-100">
-                        <div class="card-body">
-                            <h1 className='text-5xl font-bold text-center mb-10'>Login</h1>
-                            <div class="form-control mb-5">
-                                <label class="label">
-                                    <span class="label-text text-xl">Email</span>
-                                </label>
-                                <input type="text" placeholder="Email" class="input input-bordered text-xl" />
-                            </div>
-                            <div class="form-control">
-                                <label class="label">
-                                    <span class="label-text text-xl">Password</span>
-                                </label>
-                                <input type="text" placeholder="Password" class="input input-bordered text-xl" />
+                        <form onSubmit={handleLogin}>
+                            <div class="card-body">
+                                <h1 className='text-5xl font-bold text-center mb-10'>Login</h1>
+                                <div class="form-control mb-5">
+                                    <label class="label">
+                                        <span class="label-text text-xl">Email</span>
+                                    </label>
+                                    <input type="text" placeholder="Email" name='email' class="input input-bordered text-xl" required />
+                                </div>
+                                <div class="form-control">
+                                    <label class="label">
+                                        <span class="label-text text-xl">Password</span>
+                                    </label>
+                                    <input type="text" placeholder="Password" name='password' class="input input-bordered text-xl" required />
 
-                                {/* Forget password area  */}
-                                <div className=''>
+                                    {/* Forget password area  */}
+                                    <div className=''>
 
-                                    {
-                                        forgetPass && <input onBlur={() => setForgetPass(false)} value={forgetPassText} onChange={(e) => setForgetPassText(e.target.value)} type="text" placeholder="Enter your Email" class="mt-5 input input-bordered text-xl w-full" />
-                                    }
-                                    <br />
-                                    <label class="my-2">
-                                        <button onClick={() => setForgetPass(true)} class=" hover:text-primary text-xl"><span >Forgot password?</span></button>
+                                        {
+                                            forgetPass && <input onBlur={() => setForgetPass(false)} value={forgetPassText} onChange={(e) => setForgetPassText(e.target.value)} type="text" placeholder="Enter your Email" class="mt-5 input input-bordered text-xl w-full" />
+                                        }
+                                        <br />
+                                        <label class="my-2">
+                                            <button onClick={() => setForgetPass(true)} class=" hover:text-primary text-xl"><span >Forgot password?</span></button>
+                                        </label>
+                                    </div>
+                                    <label class="">
+                                        <Link to="/register" class=" pointer hover:text-primary  text-xl">New here? Sign Up
+                                        </Link>
                                     </label>
                                 </div>
-                                <label class="">
-                                    <Link to="/register" class=" pointer hover:text-primary  text-xl">New here? Sign Up
-                                    </Link>
-                                </label>
+
+                                {/* Error Shows here */}
+                                <div>
+                                    {
+                                        passwordError && <p className='text-red-500'>{passwordError}</p>
+                                    }
+                                </div>
+                                <div class="form-control mt-6">
+                                    <button type="submit" class="btn btn-accent font-bold text-lg text-white">Login</button>
+                                </div>
+
+                                <div class="flex flex-col w-full border-opacity-50">
+                                    <div class="divider">OR</div>
+                                </div>
+                                <div class="form-control ">
+                                    <button class="btn bg-white text-black hover:text-white  font-bold text-lg ">Continue With Google</button>
+                                </div>
                             </div>
-                            <div class="form-control mt-6">
-                                <button class="btn btn-accent font-bold text-lg text-white">Login</button>
-                            </div>
-                            <div class="flex flex-col w-full border-opacity-50">
-                                <div class="divider">OR</div>
-                            </div>
-                            <div class="form-control ">
-                                <button class="btn bg-white text-black hover:text-white  font-bold text-lg ">Continue With Google</button>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
