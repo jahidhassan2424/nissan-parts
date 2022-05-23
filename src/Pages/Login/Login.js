7d9f7d111326ce505786454740c9c18803eb8a9d
Jahid Hassan <jahidhassan.programmer@gmail.com>
2022-05-23 08:29:27 +0600

    before password strength bar

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
-
+import { useForm } from "react-hook-form";
 const Login = () => {
     const [forgetPass, setForgetPass] = useState(false);
     const [forgetPassText, setForgetPassText] = useState("");
     const [passwordError, setPasswordError] = useState("");
+
+    // React hook forms element
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
     const [
         signInWithEmailAndPassword,
         user,
@@ -19,33 +23,42 @@ const Login = () => {
         return <Loading></Loading>
     }
     error && setPasswordError(error.message);
-    const handleLogin = (e) => {
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
+    const onSubmit = data => {
+        console.log(data)
+        const email = data.email;
+        const password = data.password;
+    };
 
-        signInWithEmailAndPassword(email, password);
-
-    }
     return (
         <div>
             <div class=" flex justify-center items-center mt-20">
                 <div class=" w-1/4 flex-col lg:flex-row-reverse">
                     <div class="card flex-shrink-0 w-full  shadow-2xl bg-base-100">
-                        <form onSubmit={handleLogin}>
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <div class="card-body">
                                 <h1 className='text-5xl font-bold text-center mb-10'>Login</h1>
-                                <div class="form-control mb-5">
+                                <div class="form-control ">
                                     <label class="label">
                                         <span class="label-text text-xl">Email</span>
                                     </label>
-                                    <input type="text" placeholder="Email" name='email' class="input input-bordered text-xl" required />
+
+                                    <input type="email" placeholder="Email" name='email' class="input input-bordered text-xl" {...register("email",
+                                        { required: true })} />
+                                    <label class="label">
+                                        {errors?.email?.type === 'required' && <span class="label-text-alt text-lg">{errors.email.message}</span>}
+                                    </label>
+
                                 </div>
                                 <div class="form-control">
                                     <label class="label">
                                         <span class="label-text text-xl">Password</span>
                                     </label>
-                                    <input type="text" placeholder="Password" name='password' class="input input-bordered text-xl" required />
+
+                                    <input type="text" placeholder="Password" name='password' class="input input-bordered text-xl" {...register("password",
+                                        {
+                                            required: true,
+                                        })} />
+
 
                                     {/* Forget password area  */}
                                     <div className=''>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,47 +1,81 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+        const name = data.name;
+        const email = data.email;
+        const password = data.password;
+        // createUserWithEmailAndPassword(email, password)
+        //     .then(res => console.log(res))
+
+    };
     return (
         <div>
             <div class=" flex justify-center items-center mt-20">
                 <div class=" w-1/4 flex-col lg:flex-row-reverse">
                     <div class="card flex-shrink-0 w-full  shadow-2xl bg-base-100">
-                        <div class="card-body">
-                            <h1 className='text-5xl font-bold text-center mb-10'>Register</h1>
-                            <div class="form-control mb-2">
-                                <label class="label">
-                                    <span class="label-text text-xl">Name</span>
-                                </label>
-                                <input type="text" placeholder="Your Name" class="input input-bordered text-xl" />
-                            </div>
-                            <div class="form-control mb-2">
-                                <label class="label">
-                                    <span class="label-text text-xl">Email</span>
-                                </label>
-                                <input type="text" placeholder="Email" class="input input-bordered text-xl" />
-                            </div>
-                            <div class="form-control">
-                                <label class="label">
-                                    <span class="label-text text-xl">Password</span>
-                                </label>
-                                <input type="text" placeholder="Password" class="input input-bordered text-xl" />
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div class="card-body">
+                                <h1 className='text-5xl font-bold text-center mb-10'>Register</h1>
+                                <div class="form-control mb-2">
+                                    <label class="label">
+                                        <span class="label-text text-xl">Name</span>
+                                    </label>
+                                    <input type="text" placeholder="Your Name" class="input input-bordered text-xl" {...register("name",
+                                        { required: true })} />
+                                </div>
+                                <div class="form-control mb-2">
+                                    <label class="label">
+                                        <span class="label-text text-xl">Email</span>
+                                    </label>
+                                    <input type="text" placeholder="Email" class="input input-bordered text-xl"
+                                        {...register("email",
+                                            { required: true })} />
+                                </div>
+                                <div class="form-control">
+                                    <label class="label">
+                                        <span class="label-text text-xl">Password</span>
+                                    </label>
+                                    <input type="text" placeholder="Password" name='password' class="input input-bordered text-xl" {...register("password",
+                                        {
+                                            required: true,
+                                            pattern: /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+                                        })} />
+                                    <label class="label">
+                                        {errors.password?.type == "pattern" && <span class="label-text-alt text-red-500 text-lg">Password must be atleast 6 characters containing both capital and small letter, a Number, 1 special character</span>}
 
-                                <label class="mt-5">
-                                    <Link to="/login" class=" pointer hover:text-primary  text-xl">Already have an account? Login
-                                    </Link>
-                                </label>
+                                        {errors?.password?.type === 'required' && <span class="label-text-alt text-red-500 text-lg">Password is Required</span>}
+                                    </label>
+
+                                    <label class="mt-5">
+                                        <Link to="/login" class=" pointer hover:text-primary text-xl">Already have an account? Login
+                                        </Link>
+                                    </label>
+                                </div>
+                                <div class="form-control mt-6">
+                                    <button class="btn btn-accent font-bold text-lg text-white uppercase">Register</button>
+                                </div>
+                                <div class="flex flex-col w-full border-opacity-50">
+                                    <div class="divider">OR</div>
+                                </div>
+                                <div class="form-control ">
+                                    <button class="btn bg-white text-black hover:text-white  font-bold text-lg ">Continue With Google</button>
+                                </div>
                             </div>
-                            <div class="form-control mt-6">
-                                <button class="btn btn-accent font-bold text-lg text-white uppercase">Register</button>
-                            </div>
-                            <div class="flex flex-col w-full border-opacity-50">
-                                <div class="divider">OR</div>
-                            </div>
-                            <div class="form-control ">
-                                <button class="btn bg-white text-black hover:text-white  font-bold text-lg ">Continue With Google</button>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
